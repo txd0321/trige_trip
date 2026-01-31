aa8da004b2b0f7d5d31f391a70141bc574155293
txd0321 <55260310+txd0321@users.noreply.github.com>
2026-02-01 00:21:05 +0800

    ver.0201

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -10,52 +10,83 @@ Page({
     showPlaceholder: true,
 
     randomActions: ['catdance', 'catspeak', 'catyawn'],
-    idleTimer: null,
+
     isPlayingAction: false,
     musicOn: true,
   },
 
   /* ------------------------- 生命周期 ------------------------- */
+  onReady() {
+    this.videoCtx = wx.createVideoContext('elfVideo', this);
+  },
+
   onLoad() {
-    this.startIdleLoop();
+    // 进入页面立即播放一次 catlook，然后进入闲置循环
+    this.playCatlook();
+  },
+  onHide() {
+    // 切换到其他 Tab 时会触发 onHide，但不会销毁页面
+    clearTimeout(this.idleTimer);
+    if (this.videoCtx) {
+      this.videoCtx.stop(); // 彻底停止，释放声音
+    }
+    // 清空状态，返回时重新播放
+    this.setData({ currentSrc: '', showPlaceholder: true, isPlayingAction: false });
+    this.wasHidden = true;
+  },
+  onShow() {
+    // 第一次 onShow 会在 onLoad 之后立即触发，此时由 onLoad 负责首帧播放
+    if (this.wasHidden) {
+      this.wasHidden = false;
+      this.playCatlook();
+    }
   },
   onUnload() {
-    clearTimeout(this.data.idleTimer);
+    clearTimeout(this.idleTimer);
   },
 
-  startIdleLoop() {
-    const loop = () => {
-      this.playOnce(this.data.basePath + 'catlook.mp4');
-      this.data.idleTimer = setTimeout(loop, 10000);
-    };
-    this.data.idleTimer = setTimeout(loop, 1000);
+  /* ------------------ 播放控制 ------------------ */
+  // 播放一次 catlook
+  playCatlook() {
+    this.setData({ showPlaceholder: true, currentSrc: this.data.basePath + 'catlook.mp4', isLoop: false });
   },
 
+  // 安排 10 秒后进入一次 catlook 播放
+  scheduleIdle() {
+    clearTimeout(this.idleTimer);
+    this.idleTimer = setTimeout(() => {
+      this.playCatlook();
+    }, 10000);
+  },
+
+  // 播放任意一次性动画（交互/idle）
   playOnce(src) {
     this.setData({ showPlaceholder: true, currentSrc: src, isLoop: false });
   },
 
   handleTap() {
     if (this.data.isPlayingAction) return;
-    clearTimeout(this.data.idleTimer);
+    clearTimeout(this.idleTimer);
     const next = this.data.randomActions[Math.floor(Math.random() * this.data.randomActions.length)];
     this.setData({ isPlayingAction: true });
     this.playOnce(this.data.basePath + `${next}.mp4`);
   },
 
   onVideoLoaded() {
-    const video = wx.createVideoContext('elfVideo', this);
-    video.seek(0);
-    video.play();
+    if (this.videoCtx) {
+      this.videoCtx.seek(0);
+      this.videoCtx.play();
+    }
   },
   onVideoPlay() {
     this.setData({ showPlaceholder: false });
-    if (this.data.isPlayingAction) {
-      setTimeout(() => {
-        this.setData({ showPlaceholder: true, currentSrc: '', isPlayingAction: false });
-        this.startIdleLoop();
-      }, 4800);
-    }
+  },
+
+  onVideoEnded() {
+    // 无论是 catlook 还是点击动作，播放完都回到 catstop
+    this.setData({ showPlaceholder: true, currentSrc: '', isPlayingAction: false });
+    // 进入 idle：catstop 停留 10s 后再播放一次 catlook
+    this.scheduleIdle();
   },
 
   toggleMusic(e) {
